cb01ff34ea7a364fabf958400b778f7d646c2c77
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-05-01 14:54:41 +0100

    gitignore

--- cca.py
+++ cca.py
@@ -45,6 +45,11 @@ class Tracker:
 class DeltaEigenGame(Tracker, CCAEigenGame):
     pass
 
+class DeltaEigenGameSO(Tracker, CCAEigenGame):
+    def grads(self, views, u=None):
+        Aw, Bw, wAw, wBw = self._get_terms(views, u)
+        grads = 2 * Aw - (Aw @ wBw * np.sign(np.diag(wAw)) + Bw @ wAw)
+        return -grads
 
 class GHAGEP(Tracker, CCAGHAGEP):
     pass
