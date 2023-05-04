39c5c7942b613aef9d28e321784d9891ac686f2a
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-05-04 12:01:35 +0100

    gitignore

--- pls.py
+++ pls.py
@@ -73,7 +73,7 @@ class Subspace(Tracker, PLSEigenGame):
 
 class GHAGEP(Tracker, PLSGHAGEP):
     def grads(self, views, u=None):
-        Aw, Bw, wAw, wBw = self._get_terms(views, u)
+        Aw, Bw, wAw, wBw = self._get_terms(views, u, unbiased=True)
         grads = 2 * Aw - Bw @ np.triu(wAw)
         return -grads
 
@@ -96,7 +96,7 @@ class StochasticPower(Tracker, PLSGHAGEP):
 
 class SGHA(Tracker, PLSGHAGEP):
     def grads(self, views, u=None):
-        Aw, Bw, wAw, wBw = self._get_terms(views, u)
+        Aw, Bw, wAw, wBw = self._get_terms(views, u, unbiased=True)
         return Bw @ wAw - Aw
 
 
