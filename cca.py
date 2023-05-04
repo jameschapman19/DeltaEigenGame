592ddac8f33abc91a6b848d4fb09d2ed673eac73
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-05-04 17:53:11 +0100

    gitignore

--- cca.py
+++ cca.py
@@ -73,7 +73,7 @@ class GHAGEP(Tracker, CCAGHAGEP):
 class SGHA(Tracker, CCAGHAGEP):
     def grads(self, views, u=None):
         Aw, Bw, wAw, wBw = self._get_terms(views, u, unbiased=True)
-        grads = Aw - Bw @ wAw
+        grads = 2*Aw - 2*Bw @ wAw
         return -grads
 
 
