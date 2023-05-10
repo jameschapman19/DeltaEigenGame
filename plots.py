a454bfe565a8c1d3d8a9def138912732888b67fc
jameschapman19 <james.chapman.19@ucl.ac.uk>
2023-05-10 17:56:28 +0100

    gitignore

--- plots.py
+++ plots.py
@@ -173,5 +173,5 @@ for data in ["mnist", "cifar", "mediamill"]:
     for batch_size in [100]:
         for lr in [0.0001,0.001, 0.01]:
             plot_pvc(data=data, batch_size=batch_size, momentum=0, lr=lr)
-            #plot_pcc(data=data, batch_size=batch_size, momentum=0, lr=lr)
+            plot_pcc(data=data, batch_size=batch_size, momentum=0, lr=lr)
 
